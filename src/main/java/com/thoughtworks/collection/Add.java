35f50029c2b17249c92663680def0e0ac6ad7084
Kong Fanyue <fykong@thoughtworks.com>
2019-06-25 23:29:34 +0800

    First part of add test

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -1,51 +1,83 @@
 package com.thoughtworks.collection;
 
+import java.util.List;
+import java.util.stream.IntStream;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-import java.util.List;
+class Add {
+    int getSumOfEvens(int leftBorder, int rightBorder) {
+        int min = getMin(leftBorder, rightBorder);
+        int max = getMax(leftBorder, rightBorder);
+        return IntStream.rangeClosed(min, max)
+                .filter(this::isEvenNumber)
+                .sum();
+    }
 
-public class Add {
-    public int getSumOfEvens(int leftBorder, int rightBorder) {
+    private int getMin(int leftBorder, int rightBorder) {
+        if (leftBorder < rightBorder) {
+            return leftBorder;
+        }
+
+        return rightBorder;
+    }
+
+    private int getMax(int leftBorder, int rightBorder) {
+        if (leftBorder > rightBorder) {
+            return leftBorder;
+        }
+
+        return rightBorder;
+    }
+
+    private boolean isEvenNumber(int number) {
+        return number % 2 == 0;
+    }
+
+    int getSumOfOdds(int leftBorder, int rightBorder) {
+        int min = getMin(leftBorder, rightBorder);
+        int max = getMax(leftBorder, rightBorder);
+        return IntStream.rangeClosed(min, max)
+                .filter(this::isOddNumber)
+                .sum();
+    }
+
+    private boolean isOddNumber(int number) {
+        return number % 2 != 0;
+    }
+
+    int getSumTripleAndAddTwo(List<Integer> arrayList) {
+        return arrayList.stream().mapToInt(integer -> integer * 3 + 2).sum();
+    }
+
+    List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public int getSumOfOdds(int leftBorder, int rightBorder) {
+    int getSumOfProcessedOdds(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public int getSumTripleAndAddTwo(List<Integer> arrayList) {
+    double getMedianOfEven(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
+    double getAverageOfEven(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public int getSumOfProcessedOdds(List<Integer> arrayList) {
+    boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
         throw new NotImplementedException();
     }
 
-    public double getMedianOfEven(List<Integer> arrayList) {
+    List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public double getAverageOfEven(List<Integer> arrayList) {
+    List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 
-    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
-
-    public List<Integer> getProcessedList(List<Integer> arrayList) {
+    List<Integer> getProcessedList(List<Integer> arrayList) {
         throw new NotImplementedException();
     }
 }

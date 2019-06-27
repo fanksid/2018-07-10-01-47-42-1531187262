5a993885b6169197796a65e9833c78f41f739603
Kong Fanyue <fykong@thoughtworks.com>
2019-06-27 11:37:38 +0800

    Finish Flaten task

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -1,21 +1,22 @@
 package com.thoughtworks.collection;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Flaten {
 
     Integer[][] array;
+
     public Flaten(Integer[][] array) {
         this.array = array;
     }
 
-    public List<Integer> transformToOneDimesional() {
-        throw new NotImplementedException();
+    public List<Integer> transformToOneDimensional() {
+        return Arrays.stream(array).flatMap(Arrays::stream).collect(Collectors.toList());
     }
 
-    public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+    public List<Integer> transformToUnrepeatedOneDimensional() {
+        return Arrays.stream(array).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
     }
 }
